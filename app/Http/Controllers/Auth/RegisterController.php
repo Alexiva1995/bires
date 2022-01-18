f7d978be1b6e128f79a27d0c958d76e0482b5ec7
William Ache <william28ache@gmail.com>
2022-01-18 18:55:48 -0400

    email config

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -58,7 +58,6 @@ class RegisterController extends Controller
             'country' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
-            'password_confirmation' => ['required', 'string', 'min:8', 'confirmed'],
         ]);
 
     }
