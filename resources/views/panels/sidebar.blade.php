63ff82567f98e3c1578dd2a6f696509114e4d148
Abraham-maker <imnoteverywhere12@gmail.com>
2022-03-03 17:37:29 -0400

    separamos el sidebar del user como del admin

--- resources/views/panels/sidebar.blade.php
+++ resources/views/panels/sidebar.blade.php
@@ -45,6 +45,8 @@ $configData = Helper::applClasses();
   <div class="shadow-bottom"></div>
   <div class="main-menu-content">
     <ul class="navigation navigation-main" id="main-menu-navigation" data-menu="menu-navigation">
+      {{-- Para Usuarios Normales --}}
+      @if (Auth::user()->admin == 0)
       {{-- Foreach menu item starts --}}
       @if(isset($menuData[0]))
       @foreach($menuData[0]->menu as $menu)
@@ -61,13 +63,13 @@ $configData = Helper::applClasses();
       $custom_classes = $menu->classlist;
       }
       @endphp
-      <li class="nav-item {{ $custom_classes }} {{Route::currentRouteName() === $menu->slug ? 'active' : ''}}">
+      <li class="nav-item {{ Route::currentRouteName() === $menu->slug ? 'active' : '' }} {{ $custom_classes }}">
         <a href="{{isset($menu->url)? url($menu->url):'javascript:void(0)'}}" class="d-flex align-items-center" target="{{isset($menu->newTab) ? '_blank':'_self'}}">
           <i data-feather="{{ $menu->icon }}"></i>
           <span class="menu-title text-truncate">{{ __('locale.'.$menu->name) }}</span>
           @if (isset($menu->badge))
-          <?php $badgeClasses = "badge rounded-pill badge-light-primary ms-auto me-1" ?>
-          <span class="{{ isset($menu->badgeClass) ? $menu->badgeClass : $badgeClasses }}">{{$menu->badge}}</span>
+          <?php $badgeClasses = "badge badge-pill badge-light-primary ml-auto mr-1" ?>
+          <span class="{{ isset($menu->badgeClass) ? $menu->badgeClass : $badgeClasses }} ">{{$menu->badge}}</span>
           @endif
         </a>
         @if(isset($menu->submenu))
@@ -77,6 +79,48 @@ $configData = Helper::applClasses();
       @endif
       @endforeach
       @endif
+      {{-- Foreach menu item ends --}}
+
+      {{-- para usuarios admin --}}
+      @else
+
+      {{-- Foreach menu item starts --}}
+      @if(isset($menuData[1]))
+      @foreach($menuData[1]->menu as $menu)
+      @if(isset($menu->navheader))
+      <li class="navigation-header">
+        <span>{{ __('locale.'.$menu->navheader) }}</span>
+        <i data-feather="more-horizontal"></i>
+      </li>
+      @else
+      {{-- Add Custom Class with nav-item --}}
+      @php
+      $custom_classes = "";
+      if(isset($menu->classlist)) {
+      $custom_classes = $menu->classlist;
+      }
+      @endphp
+      <li class="nav-item {{ Route::currentRouteName() === $menu->slug ? 'active' : '' }} {{ $custom_classes }}">
+        <a href="{{isset($menu->url)? url($menu->url):'javascript:void(0)'}}" class="d-flex align-items-center" target="{{isset($menu->newTab) ? '_blank':'_self'}}">
+          <i data-feather="{{ $menu->icon }}"></i>
+          <span class="menu-title text-truncate">{{ __('locale.'.$menu->name) }}</span>
+          @if (isset($menu->badge))
+          <?php $badgeClasses = "badge badge-pill badge-light-primary ml-auto mr-1" ?>
+          <span class="{{ isset($menu->badgeClass) ? $menu->badgeClass : $badgeClasses }} ">{{$menu->badge}}</span>
+          @endif
+        </a>
+        @if(isset($menu->submenu))
+        @include('panels/submenu', ['menu' => $menu->submenu])
+
+        @endif
+      </li>
+
+
+      @endif
+      @endforeach
+      @endif
+      @endif
+
       {{-- Foreach menu item ends --}}
     </ul>
   </div>
