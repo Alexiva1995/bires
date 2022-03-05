9da9e68e83a3c25ff6a6bd47cc61585db6f76110
Cesar Montevideo <77177023+cesar899@users.noreply.github.com>
2022-03-05 09:02:44 -0400

    Update index.blade.php

--- resources/views/plans/index.blade.php
+++ resources/views/plans/index.blade.php
@@ -34,38 +34,284 @@
 </style>
 
 @section('content')
+<body style="background: radial-gradient(50% 50% at 50% 50%, #05103A 0%, #05103A 100%);">
     <div class="content-body">
         <h3 class="card-header mb-2"><strong>Planes</strong></strong></h3>
         <div class="container">
             <div class="row">
-                @foreach ($plans as $item)
-                    <div class="card container-fluid" style="width: 13rem; padding:4px">
-                        <div class="text-center mt-1">
-                            <font size="4"><strong>{{$item->name}}</strong></font>
-                        </div>
-                        <div style="flex: 1 1 auto;padding: 1rem 1rem;">
-                            <p class="text-center" style="font-size: 13px; color: white;">
-                                <strong>{{$item->Description}}</strong>
-                            </p>
+               <div class="card container-fluid" style="width: 13rem; padding:14px">
+                   <div class="text-center mt-1">
+                        <p size="4">
+                            <strong style=" font-size:21px; height: 25px;width: 135px;left: 7.5px;top: 0px;border-radius: nullpx;">  Plan Inicial
+                            </strong>
+                        </p>
+                    </div>
+                    <div style="flex: 1 1 auto;padding: 0rem 0.5rem;">
+                        <p class="text-center" style="height: 45px;width: 150px;top: 46px;border-radius: nullpx;">
+                            <strong style="font-size: 14px;font-style: normal;font-weight: 400;line-height: 20px;letter-spacing: 0.0075em;">
+                                Ideal para iniciar y tener acceso a la academia y servicios.
+                            </strong>
+                        </p>
 
-                            <div style="font-size: 11px">
-                                <strong>Retorno:</strong> 
-                                <strong style="color: rgba(117, 202, 255, 1); font-size: 12px">
-                                    7.5% por mes
+                        <div class="mt-3" style="font-size: 11px">
+                            <img class="img-fluid card-img-size " src={{asset('images/zoe/retorno.png')}} alt="retorno">
+                        </div>
+                        <br>
+                        <div class="d-grid gap-4 col-20 mx-auto mt-2">
+                            <a href="#" target="_blank" class="btn" style=" margin-bottom: 13%; width: 150px;">
+                                <strong style="height: 19px;width: 83px;left: 21px;top: 5px;border-radius: nullpx;">
+                                    100 ZOEC
                                 </strong>
-                            </div>
-                            <br>
-                            <div class="d-grid gap-4 col-20 mx-auto">
-                                <a href="#" target="_blank" class="btn">
-                                   <strong style="font-style: normal;font-weight: 600;font-size: 13.5px;line-height: 105%;">
-                                       {{ $item->price }} ZOEC
-                                   </strong>
-                                </a>
-                            </div>
+                            </a>
+                        </div>
+                    </div> 
+                </div>
+                 <div class="card container-fluid" style="width: 13rem; padding:14px">
+                   <div class="text-center mt-1">
+                        <p size="4">
+                            <strong style=" font-size:21px; height: 25px;width: 135px;left: 7.5px;top: 0px;border-radius: nullpx;"> 
+                                Plan Pro
+                            </strong>
+                        </p>
+                    </div>
+                    <div style="flex: 1 1 auto;padding: 0rem 0.5rem;">
+                        <p class="text-center" style="height: 45px;width: 150px;top: 46px;border-radius: nullpx;">
+                            <strong style="font-size: 14px;font-style: normal;font-weight: 400;line-height: 20px;letter-spacing: 0.0075em;">
+                                Ideal para iniciar y tener acceso a la academia y servicios.
+                            </strong>
+                        </p>
+
+                        <div class="mt-3" style="font-size: 11px">
+                            <img class="img-fluid card-img-size " src={{asset('images/zoe/retorno.png')}} alt="retorno">
+                        </div>
+                        <br>
+                        <div class="d-grid gap-4 col-20 mx-auto mt-2">
+                            <a href="#" target="_blank" class="btn" style=" margin-bottom: 13%; width: 150px;">
+                                <strong style="height: 19px;width: 83px;left: 21px;top: 5px;border-radius: nullpx;">
+                                    200 ZOEC
+                                </strong>
+                            </a>
+                        </div>
+                    </div> 
+                </div>
+                <div class="card container-fluid" style="width: 13rem; padding:14px">
+                   <div class="text-center mt-1">
+                        <p size="4">
+                            <strong style=" font-size:21px; height: 25px;width: 135px;left: 7.5px;top: 0px;border-radius: nullpx;">  Plan Business
+                            </strong>
+                        </p>
+                    </div>
+                    <div style="flex: 1 1 auto;padding: 0rem 0.5rem;">
+                        <p class="text-center" style="height: 45px;width: 150px;top: 46px;border-radius: nullpx;">
+                            <strong style="font-size: 14px;font-style: normal;font-weight: 400;line-height: 20px;letter-spacing: 0.0075em;">
+                                Ideal para iniciar y tener acceso a la academia y servicios.
+                            </strong>
+                        </p>
+
+                        <div class="mt-3" style="font-size: 11px">
+                            <img class="img-fluid card-img-size " src={{asset('images/zoe/retorno.png')}} alt="retorno">
+                        </div>
+                        <br>
+                        <div class="d-grid gap-4 col-20 mx-auto">
+                            <a href="#" target="_blank" class="btn" style=" margin-bottom: 13%; width: 150px;">
+                                <strong style="height: 19px;width: 83px;left: 21px;top: 5px;border-radius: nullpx;">
+                                    1200 ZOEC
+                                </strong>
+                            </a>
+                        </div>
+                    </div> 
+                </div>
+                <div class="card container-fluid" style="width: 13rem; padding:14px">
+                   <div class="text-center mt-1">
+                        <p size="4">
+                            <strong style=" font-size:21px; height: 25px;width: 135px;left: 7.5px;top: 0px;border-radius: nullpx;">  Plan Lider
+                            </strong>
+                        </p>
+                    </div>
+                   <div style="flex: 1 1 auto;padding: 0rem 0.5rem;">
+                        <p class="text-center" style="height: 45px;width: 150px;top: 46px;border-radius: nullpx;">
+                            <strong style="font-size: 14px;font-style: normal;font-weight: 400;line-height: 20px;letter-spacing: 0.0075em;">
+                                Ideal para iniciar y tener acceso a la academia y servicios.
+                            </strong>
+                        </p>
+
+                        <div class="mt-3" style="font-size: 11px">
+                            <img class="img-fluid card-img-size " src={{asset('images/zoe/retorno.png')}} alt="retorno">
+                        </div>
+                        <br>
+                        <div class="d-grid gap-4 col-20 mx-auto mt-2">
+                            <a href="#" target="_blank" class="btn" style=" margin-bottom: 13%; width: 150px;">
+                                <strong style="height: 19px;width: 83px;left: 21px;top: 5px;border-radius: nullpx;">
+                                    2000 ZOEC
+                                </strong>
+                            </a>
                         </div>
                     </div>
-                @endforeach
+                </div>
+                <div class="card container-fluid" style="width: 13rem; padding:14px">
+                   <div class="text-center mt-1">
+                        <p size="4">
+                            <strong style=" font-size:21px; height: 25px;width: 135px;left: 7.5px;top: 0px;border-radius: nullpx;">  Plan Wolf
+                            </strong>
+                        </p>
+                    </div>
+                   <div style="flex: 1 1 auto;padding: 0rem 0.5rem;">
+                        <p class="text-center" style="height: 45px;width: 150px;top: 46px;border-radius: nullpx;">
+                            <strong style="font-size: 14px;font-style: normal;font-weight: 400;line-height: 20px;letter-spacing: 0.0075em;">
+                                Ideal para iniciar y tener acceso a la academia y servicios.
+                            </strong>
+                        </p>
+
+                        <div class="mt-3" style="font-size: 11px">
+                            <img class="img-fluid card-img-size " src={{asset('images/zoe/retorno.png')}} alt="retorno">
+                        </div>
+                        <br>
+                        <div class="d-grid gap-4 col-20 mx-auto mt-2">
+                            <a href="#" target="_blank" class="btn" style=" margin-bottom: 13%; width: 150px;">
+                                <strong style="height: 19px;width: 83px;left: 21px;top: 5px;border-radius: nullpx;">
+                                    3000 ZOEC
+                                </strong>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="card container-fluid" style="width: 13rem; padding:14px">
+                   <div class="text-center mt-1">
+                        <p size="4">
+                            <strong style=" font-size:21px; height: 25px;width: 135px;left: 7.5px;top: 0px;border-radius: nullpx;">  Plan Inicial
+                            </strong>
+                        </p>
+                    </div>
+                    <div style="flex: 1 1 auto;padding: 0rem 0.5rem;">
+                        <p class="text-center" style="height: 45px;width: 150px;top: 46px;border-radius: nullpx;">
+                            <strong style="font-size: 14px;font-style: normal;font-weight: 400;line-height: 20px;letter-spacing: 0.0075em;">
+                                Ideal para iniciar y tener acceso a la academia y servicios.
+                            </strong>
+                        </p>
+
+                        <div class="mt-3" style="font-size: 11px">
+                            <img class="img-fluid card-img-size " src={{asset('images/zoe/retorno.png')}} alt="retorno">
+                        </div>
+                        <br>
+                        <div class="d-grid gap-4 col-20 mx-auto mt-2">
+                            <a href="#" target="_blank" class="btn" style=" margin-bottom: 13%; width: 150px;">
+                                <strong style="height: 19px;width: 83px;left: 21px;top: 5px;border-radius: nullpx;">
+                                    5000 ZOEC
+                                </strong>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="card container-fluid" style="width: 13rem; padding:14px">
+                   <div class="text-center mt-1">
+                        <p size="4">
+                            <strong style=" font-size:21px; height: 25px;width: 135px;left: 7.5px;top: 0px;border-radius: nullpx;">  Plan Inicial
+                            </strong>
+                        </p>
+                    </div>
+                   <div style="flex: 1 1 auto;padding: 0rem 0.5rem;">
+                        <p class="text-center" style="height: 45px;width: 150px;top: 46px;border-radius: nullpx;">
+                            <strong style="font-size: 14px;font-style: normal;font-weight: 400;line-height: 20px;letter-spacing: 0.0075em;">
+                                Ideal para iniciar y tener acceso a la academia y servicios.
+                            </strong>
+                        </p>
+
+                        <div class="mt-3" style="font-size: 11px">
+                            <img class="img-fluid card-img-size " src={{asset('images/zoe/retorno.png')}} alt="retorno">
+                        </div>
+                        <br>
+                        <div class="d-grid gap-4 col-20 mx-auto mt-2">
+                            <a href="#" target="_blank" class="btn" style=" margin-bottom: 13%; width: 150px;">
+                                <strong style="height: 19px;width: 83px;left: 21px;top: 5px;border-radius: nullpx;">
+                                    10.000 ZOEC
+                                </strong>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="card container-fluid" style="width: 13rem; padding:14px">
+                   <div class="text-center mt-1">
+                        <p size="4">
+                            <strong style=" font-size:21px; height: 25px;width: 135px;left: 7.5px;top: 0px;border-radius: nullpx;">  Plan Inicial
+                            </strong>
+                        </p>
+                    </div>
+                   <div style="flex: 1 1 auto;padding: 0rem 0.5rem;">
+                        <p class="text-center" style="height: 45px;width: 150px;top: 46px;border-radius: nullpx;">
+                            <strong style="font-size: 14px;font-style: normal;font-weight: 400;line-height: 20px;letter-spacing: 0.0075em;">
+                                Ideal para iniciar y tener acceso a la academia y servicios.
+                            </strong>
+                        </p>
+
+                        <div class="mt-3" style="font-size: 11px">
+                            <img class="img-fluid card-img-size " src={{asset('images/zoe/retorno.png')}} alt="retorno">
+                        </div>
+                        <br>
+                        <div class="d-grid gap-4 col-20 mx-auto mt-2">
+                            <a href="#" target="_blank" class="btn" style=" margin-bottom: 13%; width: 150px;">
+                                <strong style="height: 19px;width: 83px;left: 21px;top: 5px;border-radius: nullpx;">
+                                    30.000 ZOEC
+                                </strong>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="card container-fluid" style="width: 13rem; padding:14px">
+                   <div class="text-center mt-1">
+                        <p size="4">
+                            <strong style=" font-size:21px; height: 25px;width: 135px;left: 7.5px;top: 0px;border-radius: nullpx;">  Plan Inicial
+                            </strong>
+                        </p>
+                    </div>
+                   <div style="flex: 1 1 auto;padding: 0rem 0.5rem;">
+                        <p class="text-center" style="height: 45px;width: 150px;top: 46px;border-radius: nullpx;">
+                            <strong style="font-size: 14px;font-style: normal;font-weight: 400;line-height: 20px;letter-spacing: 0.0075em;">
+                                Ideal para iniciar y tener acceso a la academia y servicios.
+                            </strong>
+                        </p>
+
+                        <div class="mt-3" style="font-size: 11px">
+                            <img class="img-fluid card-img-size " src={{asset('images/zoe/retorno.png')}} alt="retorno">
+                        </div>
+                        <br>
+                        <div class="d-grid gap-4 col-20 mx-auto mt-2">
+                            <a href="#" target="_blank" class="btn" style=" margin-bottom: 13%; width: 150px;">
+                                <strong style="height: 19px;width: 83px;left: 21px;top: 5px;border-radius: nullpx;">
+                                    50.000 ZOEC
+                                </strong>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="card container-fluid" style="width: 13rem; padding:14px">
+                   <div class="text-center mt-1">
+                        <p size="4">
+                            <strong style=" font-size:21px; height: 25px;width: 135px;left: 7.5px;top: 0px;border-radius: nullpx;">  Plan Inicial
+                            </strong>
+                        </p>
+                    </div>
+                    <div style="flex: 1 1 auto;padding: 0rem 0.5rem;">
+                        <p class="text-center" style="height: 45px;width: 150px;top: 46px;border-radius: nullpx;">
+                            <strong style="font-size: 14px;font-style: normal;font-weight: 400;line-height: 20px;letter-spacing: 0.0075em;">
+                                Ideal para iniciar y tener acceso a la academia y servicios.
+                            </strong>
+                        </p>
+
+                        <div class="mt-3" style="font-size: 11px">
+                            <img class="img-fluid card-img-size " src={{asset('images/zoe/retorno.png')}} alt="retorno">
+                        </div>
+                        <br>
+                        <div class="d-grid gap-4 col-20 mx-auto mt-2">
+                            <a href="#" target="_blank" class="btn" style=" margin-bottom: 13%; width: 150px;">
+                                <strong style="height: 19px;width: 83px;left: 21px;top: 5px;border-radius: nullpx;">
+                                    100.000 ZOEC
+                                </strong>
+                            </a>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
+<body>
 @endsection
