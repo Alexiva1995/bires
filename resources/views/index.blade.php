0b639a6cfcac3a242b8502751f0d99fc6546528d
Cesar Montevideo <77177023+cesar899@users.noreply.github.com>
2022-03-07 12:04:22 -0400

    arreglos a intercambios y landing

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -47,109 +47,107 @@
 </style>
 
 <body class="d-flex h-100 text-center text-white bg-zoe">
-
-    <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
-        <header class="mb-auto">
-            <div>
-        
-               <a href="{{ route('landing') }}" class="float-md-start mb-0">
-                   <img src="{{ asset('images/zoe/header/S.png') }}" alt="logo">
-                   <img src="{{ asset('images/zoe/header/p.png') }}" alt="logo">
-                   <img src="{{ asset('images/zoe/header/O.png') }}" alt="logo">
-                   <img src="{{ asset('images/zoe/header/R.png') }}" alt="logo">
-                   <img src="{{ asset('images/zoe/header/A.png') }}" alt="logo">
-                </a> 
-                <nav class="nav nav-masthead justify-content-center float-md-end">
-                    <a class="nav-link text-white active" aria-current="page" href="#">
-                        <strong style="height: 29px;width: 121px;left: 320px;top: 0px;border-radius: nullpx;">Inicio</strong>
-                    </a>
-                    <a class="nav-link text-white" href="#"> 
-                        <strong style="height: 29px;width: 121px;left: 320px;top: 0px;border-radius: nullpx;">PMI</strong>
-                    </a>
-                    <a class="nav-link text-white" href="#">
-                        <strong style="height: 29px;width: 121px;left: 320px;top: 0px;border-radius: nullpx;">Oficinas</strong>
-                    </a>
-                    <a class="nav-link text-white" href="#">
-                        <strong style="height: 29px;width: 121px;left: 320px;top: 0px;border-radius: nullpx;">Testimonios</strong>
-                    </a>
-                    <a class="nav-link text-white" href="#">
-                        <strong style="height: 29px;width: 121px;left: 320px;top: 0px;border-radius: nullpx;">Novedades</strong>
-                    </a>
-                    <li class="nav-item dropdown dropdown-language">
-                        <a class="nav-link dropdown-toggle" id="dropdown-flag" href="#" data-bs-toggle="dropdown"
-                            aria-haspopup="true">
-                            <i class="flag-icon flag-icon-es"></i>
-                            <span class="selected-language">
-                                @if(session()->has('locale'))
-                                {{session()->get('locale')}}
-                                @else
-                                ES
-                                @endif
-                            </span>
-                        </a>
-                        <div class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdown-flag">
-                            <a href="#googtrans(es|es)" class="dropdown-item lang-select rounded" data-language="es"
-                                alt="ESPAÑOL">
-                                <i class="flag-icon flag-icon-es"></i> ES
-                            </a>
-                            <a href="#googtrans(es|en)" class="dropdown-item lang-select rounded" data-language="en"
-                                alt="INGLES">
-                                <i class="flag-icon flag-icon-us"></i> EN
-                            </a>
-                            <a href="#googtrans(es|fr)" class="dropdown-item lang-select rounded" data-language="fr"
-                                alt="FRANCÉS">
-                                <i class="flag-icon flag-icon-fr"></i> FR
-                            </a>
-                            <a href="#googtrans(es|de)" class="dropdown-item lang-select rounded" data-language="de"
-                                alt="ALEMÁN">
-                                <i class="flag-icon flag-icon-de"></i> AL
-                            </a>
-                            <a href="#googtrans(es|pt)" class="dropdown-item lang-select rounded" data-language="pt"
-                                alt="PORTUGUÉS">
-                                <i class="flag-icon flag-icon-pt"></i> PT
-                            </a>
-                        </div>
-                    </li>
-                    <a class="nav-link" href="{{ route('login') }}">
-                        <button style="height: 32px;width: 150px;left: 647px;top: 0px;border-radius: 5px;" class="btn btn-sm ">
-                            <strong style="height: 22px;width: 71px;left: 686px;top: 3px;border-radius: nullpx;">Ingresar</strong>
-                        </button>
-                    </a>
-                </nav>
-            </div>
-        </header>
-
-        <main class="mb-auto row">
-            <div class="col-6 text-left">
-                <h1 style="font-family:  Brandon Text,-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Oxygen-Sans,Ubuntu,Cantarell,Helvetica Neue,sans-serif;;
-                height: 160px;
-                width: 998px;
-                top: 332px;
-                border-radius: nullpx;
-                font-style: normal;
-                font-weight: bold;
-                font-size: 60px;
-                line-height: 111.5%; text-align: left;">¡Somos la Nueva <br>Generación!</h1>
-                <p style="
-                font-style: normal;
-                font-weight: 300;
-                font-size: 15px;
-                line-height: 146.5%; text-align: left;">Desarrolla habilidades de liderazgo, comunicación asertiva, aprende a <br> crear equipos de alto desempeño, a gestionar tus estados emocionales,<br> a identificar y romper con las creencias limitantes.
-                </p>
-                <p class="lead d-flex justify-content-left mt-4">
-                    <a href="#" class="btn btn-outline-secondary fw-bold text-white" style="width: 34%">Conoce más</a>
-                </p>
-            </div>
-        </main>
-
-    </div>
-
-    <div id="gtx-trans" style="position: absolute; left: 196px; top: 219.094px;">
-        <div class="gtx-trans-icon"></div>
-    </div>
-</div>
     
+        <div class="cover-container d-flex w-100 h-100  mx-auto flex-column">
+           <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+            <header class="mb-auto">
+                <div>
+                    <a href="{{ route('landing') }}" class="float-md-start mb-0">
+                        <img style="margin-left:15%;" src="http://127.0.0.1:8000/images/auth/logo-spora-v2.png" height="60" width="100%" alt="">
+                      {{-- <img src="{{ asset('images/zoe/header/S.png') }}" alt="logo">
+                       <img src="{{ asset('images/zoe/header/p.png') }}" alt="logo">
+                       <img src="{{ asset('images/zoe/header/O.png') }}" alt="logo">
+                       <img src="{{ asset('images/zoe/header/R.png') }}" alt="logo">
+                       <img src="{{ asset('images/zoe/header/A.png') }}" alt="logo">--}}
+                    </a> 
+                    <nav class="nav nav-masthead justify-content-center float-md-end">
+                        <a class="nav-link text-white active" aria-current="page" href="#">
+                            <strong style="height: 29px;width: 121px;left: 320px;top: 0px;border-radius: nullpx;">Inicio</strong>
+                        </a>
+                        <a class="nav-link text-white" href="#"> 
+                            <strong style="height: 29px;width: 121px;left: 320px;top: 0px;border-radius: nullpx;">PMI</strong>
+                        </a>
+                        <a class="nav-link text-white" href="#">
+                            <strong style="height: 29px;width: 121px;left: 320px;top: 0px;border-radius: nullpx;">Oficinas</strong>
+                        </a>
+                        <a class="nav-link text-white" href="#">
+                            <strong style="height: 29px;width: 121px;left: 320px;top: 0px;border-radius: nullpx;">Testimonios</strong>
+                        </a>
+                        <a class="nav-link text-white" href="#">
+                            <strong style="height: 29px;width: 121px;left: 320px;top: 0px;border-radius: nullpx;">Novedades</strong>
+                        </a>
+                        <li class="nav-item dropdown dropdown-language">
+                            <a class="nav-link dropdown-toggle" id="dropdown-flag" href="#" data-bs-toggle="dropdown"
+                                aria-haspopup="true">
+                                <i class="flag-icon flag-icon-es rounded"></i>
+                                <span class="selected-language">
+                                    @if(session()->has('locale'))
+                                    {{session()->get('locale')}}
+                                    @else
+                                    ES
+                                    @endif
+                                </span>
+                            </a>
+                            <div class="dropdown-menu dropdown-menu-end rounded" aria-labelledby="dropdown-flag">
+                                <a href="#googtrans(es|es)" class="dropdown-item lang-select rounded" data-language="es"
+                                    alt="ESPAÑOL">
+                                    <i class="flag-icon flag-icon-es"></i> ES
+                                </a>
+                                <a href="#googtrans(es|en)" class="dropdown-item lang-select rounded" data-language="en"
+                                    alt="INGLES">
+                                    <i class="flag-icon flag-icon-us"></i> EN
+                                </a>
+                                <a href="#googtrans(es|fr)" class="dropdown-item lang-select rounded" data-language="fr"
+                                    alt="FRANCÉS">
+                                    <i class="flag-icon flag-icon-fr"></i> FR
+                                </a>
+                                <a href="#googtrans(es|de)" class="dropdown-item lang-select rounded" data-language="de"
+                                    alt="ALEMÁN">
+                                    <i class="flag-icon flag-icon-de"></i> AL
+                                </a>
+                                <a href="#googtrans(es|pt)" class="dropdown-item lang-select rounded" data-language="pt"
+                                    alt="PORTUGUÉS">
+                                    <i class="flag-icon flag-icon-pt"></i> PT
+                                </a>
+                            </div>
+                        </li>
+                        <a class="nav-link" href="{{ route('login') }}">
+                            <button style="height: 32px;width: 150px;left: 647px;top: 0px;border-radius: 5px;" class="btn btn-sm ">
+                                <strong style="height: 22px;width: 71px;left: 686px;top: 3px;border-radius: nullpx;">Ingresar</strong>
+                            </button>
+                        </a>
+                    </nav>
+                </div>
+            </header>
+
+            <main class="mb-auto row">
+                <div class="col-6 text-left">
+                    <h1 style="font-family:  Brandon Text,-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Oxygen-Sans,Ubuntu,Cantarell,Helvetica Neue,sans-serif;;
+                    height: 160px;
+                    width: 998px;
+                    top: 332px;
+                    border-radius: nullpx;
+                    font-style: normal;
+                    font-weight: bold;
+                    font-size: 60px;
+                    line-height: 111.5%; text-align: left;">¡Somos la Nueva <br>Generación!</h1>
+                    <p style="
+                    font-style: normal;
+                    font-weight: 300;
+                    font-size: 15px;
+                    line-height: 146.5%; text-align: left;">Desarrolla habilidades de liderazgo, comunicación asertiva, aprende a <br> crear equipos de alto desempeño, a gestionar tus estados emocionales,<br> a identificar y romper con las creencias limitantes.
+                    </p>
+                    <p class="lead d-flex justify-content-left mt-4">
+                        <a href="#" class="btn btn-outline-secondary fw-bold text-white" style="width: 34%">Conoce más</a>
+                    </p>
+                </div>
+            </main> 
+
+        </div>
+ 
+    <footer style="background: linear-gradient(90deg, #0F6FC3 -0.66%, #F5CD0E 100%); width: 100%; height: 7%;left: 0px;top: 864px;border-radius: 0px;" >
+    </footer>  
+    </div>
 </body>
 
 </html>
-
